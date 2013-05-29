5b487bdad89f642febe1c6fab71de93bd25fbb63
Bruno Chagas <brunochagasweb@gmail.com>
2013-05-29 00:30:37 -0300

    Added fixed header

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
   var controller = $.superscrollorama();
   // individual element tween examples
-  controller.addTween('#fade-it', TweenMax.from( $('#fade-it'), .5, {css:{opacity: 0}}));
-  controller.addTween('#fly-it', TweenMax.from( $('#fly-it'), .25, {css:{right:'1000px'}, ease:Quad.easeInOut}));
-  controller.addTween('#spin-it', TweenMax.from( $('#spin-it'), .25, {css:{opacity:0, rotation: 720}, ease:Quad.easeOut}));
-  controller.addTween('#scale-it', TweenMax.fromTo( $('#scale-it'), .25, {css:{opacity:0, fontSize:'20px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, fontSize:'240px'}, ease:Quad.easeInOut}));
-  controller.addTween('#smush-it', TweenMax.fromTo( $('#smush-it'), .25, {css:{opacity:0, 'letter-spacing':'30px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, 'letter-spacing':'-10px'}, ease:Quad.easeInOut}), 0, 100); // 100 px offset for better timing
+
+  var div = $('[data-type=fixed]');
+
+  console.log(div)
+
+  if ( $(div).length >= 1 ) {
+    var start = $(div).offset().top;
+
+    $.event.add(window, "scroll", function() {
+      var p = $(window).scrollTop();
+      $(div).css('position',((p)>start) ? 'fixed' : 'static');
+      $(div).css('top',((p)>start) ? '0px' : '');
+      $(div)[ ((p)>start) ? 'addClass' : 'removeClass' ]('fixed');
+    });
+  }
+
 });
